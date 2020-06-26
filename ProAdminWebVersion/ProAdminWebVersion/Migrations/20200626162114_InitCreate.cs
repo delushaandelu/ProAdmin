9f82ff523d8521649984786ad3df42ae275e3efd
Delushaan <mdelushaan@hotmail.com>
2020-06-27 00:48:52 +0800

    .net core webApi Created for web Version
    Manage Steudnet only

---
+++ ProAdminWebVersion/ProAdminWebVersion/Migrations/20200626162114_InitCreate.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProAdminWebVersion.Migrations
+{
+    public partial class InitCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentDetails",
+                columns: table => new
+                {
+                    Sid = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    RegId = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    FirstName = table.Column<string>(type: "varchar(200)", nullable: false),
+                    LastName = table.Column<string>(type: "varchar(200)", nullable: false),
+                    Nic = table.Column<string>(type: "varchar(200)", nullable: false),
+                    Dob = table.Column<string>(type: "varchar(200)", nullable: false),
+                    HomeAdress = table.Column<string>(type: "varchar(200)", nullable: false),
+                    HomeTell = table.Column<string>(type: "varchar(200)", nullable: false),
+                    StayAddress = table.Column<string>(type: "varchar(200)", nullable: true),
+                    StayHomeTell = table.Column<string>(type: "varchar(200)", nullable: true),
+                    FatherName = table.Column<string>(type: "varchar(200)", nullable: false),
+                    ParentsTell = table.Column<string>(type: "varchar(200)", nullable: false),
+                    OlSchool = table.Column<string>(type: "varchar(200)", nullable: false),
+                    AlSchool = table.Column<string>(type: "varchar(200)", nullable: true),
+                    OlResult = table.Column<string>(type: "varchar(200)", nullable: false),
+                    JoinedDate = table.Column<string>(type: "varchar(200)", nullable: false),
+                    Batch = table.Column<string>(type: "varchar(200)", nullable: false),
+                    Scholership = table.Column<string>(type: "varchar(200)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentDetails", x => x.Sid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentDetails");
+        }
+    }
+}
---
+++ ProAdminWebVersion/ProAdminWebVersion/Models/StudentsDetail.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAdminWebVersion.Models
+{
+    public class StudentsDetail
+    {
+        [Key]
+        public int Sid { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(200)")]
+        public string RegId { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(200)")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(200)")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(200)")]
+        public string Nic { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(200)")]
+        public string Dob { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(200)")]
+        public string HomeAdress { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(200)")]
+        public string HomeTell { get; set; }
+
+        [Column(TypeName = "varchar(200)")]
+        public string StayAddress { get; set; }
+
+        [Column(TypeName = "varchar(200)")]
+        public string StayHomeTell { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(200)")]
+        public string FatherName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(200)")]
+        public string ParentsTell { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(200)")]
+        public string OlSchool { get; set; }
+
+        [Column(TypeName = "varchar(200)")]
+        public string AlSchool { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(200)")]
+        public string  OlResult { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(200)")]
+        public string JoinedDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(200)")]
+        public string Batch { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(200)")]
+        public string Scholership { get; set; }
+
+
+
+    }
+}
