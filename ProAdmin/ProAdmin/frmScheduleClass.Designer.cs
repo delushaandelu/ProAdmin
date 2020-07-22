732e6f198e10dbb7e3230a3215edbaf3a652e0af
Delushaan <mdelushaan@hotmail.com>
2020-07-22 16:35:52 +0800

    shedule class completed

--- ProAdmin/ProAdmin/frmScheduleClass.Designer.cs
+++ ProAdmin/ProAdmin/frmScheduleClass.Designer.cs
@@ -30,8 +30,27 @@
         {
             this.tabPane1 = new DevExpress.XtraBars.Navigation.TabPane();
             this.tabNavigationPage3 = new DevExpress.XtraBars.Navigation.TabNavigationPage();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.cmbbatch = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtdate = new DevExpress.XtraEditors.TextEdit();
+            this.claclass = new DevExpress.XtraEditors.Controls.CalendarControl();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.dgvclassshedule = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.batch = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.classdate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.state = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.tabPane1)).BeginInit();
             this.tabPane1.SuspendLayout();
+            this.tabNavigationPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtdate.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.claclass.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvclassshedule)).BeginInit();
             this.SuspendLayout();
             // 
             // tabPane1
@@ -51,8 +70,138 @@
             // tabNavigationPage3
             // 
             this.tabNavigationPage3.Caption = "Manage Class Shedules";
+            this.tabNavigationPage3.Controls.Add(this.groupControl1);
             this.tabNavigationPage3.Name = "tabNavigationPage3";
             this.tabNavigationPage3.Size = new System.Drawing.Size(1140, 550);
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.dgvclassshedule);
+            this.groupControl1.Controls.Add(this.btndelete);
+            this.groupControl1.Controls.Add(this.btnsave);
+            this.groupControl1.Controls.Add(this.cmbbatch);
+            this.groupControl1.Controls.Add(this.label2);
+            this.groupControl1.Controls.Add(this.label1);
+            this.groupControl1.Controls.Add(this.txtdate);
+            this.groupControl1.Controls.Add(this.claclass);
+            this.groupControl1.Location = new System.Drawing.Point(27, 23);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(1081, 591);
+            this.groupControl1.TabIndex = 0;
+            this.groupControl1.Text = "Control";
+            // 
+            // cmbbatch
+            // 
+            this.cmbbatch.FormattingEnabled = true;
+            this.cmbbatch.Location = new System.Drawing.Point(616, 50);
+            this.cmbbatch.Name = "cmbbatch";
+            this.cmbbatch.Size = new System.Drawing.Size(204, 21);
+            this.cmbbatch.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(576, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Batch";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(289, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Class Date";
+            // 
+            // txtdate
+            // 
+            this.txtdate.Enabled = false;
+            this.txtdate.Location = new System.Drawing.Point(353, 51);
+            this.txtdate.Name = "txtdate";
+            this.txtdate.Size = new System.Drawing.Size(204, 20);
+            this.txtdate.TabIndex = 2;
+            // 
+            // claclass
+            // 
+            this.claclass.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.claclass.Location = new System.Drawing.Point(24, 54);
+            this.claclass.Name = "claclass";
+            this.claclass.Size = new System.Drawing.Size(242, 235);
+            this.claclass.SyncSelectionWithEditValue = false;
+            this.claclass.TabIndex = 0;
+            this.claclass.Click += new System.EventHandler(this.claclass_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(855, 49);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 23);
+            this.btnsave.TabIndex = 8;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Enabled = false;
+            this.btndelete.Location = new System.Drawing.Point(959, 50);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(75, 23);
+            this.btndelete.TabIndex = 9;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // dgvclassshedule
+            // 
+            this.dgvclassshedule.AllowUserToAddRows = false;
+            this.dgvclassshedule.AllowUserToDeleteRows = false;
+            this.dgvclassshedule.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgvclassshedule.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvclassshedule.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.batch,
+            this.classdate,
+            this.state});
+            this.dgvclassshedule.Location = new System.Drawing.Point(292, 101);
+            this.dgvclassshedule.Name = "dgvclassshedule";
+            this.dgvclassshedule.ReadOnly = true;
+            this.dgvclassshedule.Size = new System.Drawing.Size(742, 466);
+            this.dgvclassshedule.TabIndex = 58;
+            this.dgvclassshedule.DoubleClick += new System.EventHandler(this.dgvclassshedule_DoubleClick_1);
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Visible = false;
+            // 
+            // batch
+            // 
+            this.batch.DataPropertyName = "batch";
+            this.batch.HeaderText = "Batch";
+            this.batch.Name = "batch";
+            this.batch.ReadOnly = true;
+            // 
+            // classdate
+            // 
+            this.classdate.DataPropertyName = "classdate";
+            this.classdate.HeaderText = "Class Date";
+            this.classdate.Name = "classdate";
+            this.classdate.ReadOnly = true;
+            // 
+            // state
+            // 
+            this.state.DataPropertyName = "state";
+            this.state.HeaderText = "State";
+            this.state.Name = "state";
+            this.state.ReadOnly = true;
             // 
             // frmScheduleClass
             // 
@@ -63,6 +212,13 @@
             this.Size = new System.Drawing.Size(1140, 587);
             ((System.ComponentModel.ISupportInitialize)(this.tabPane1)).EndInit();
             this.tabPane1.ResumeLayout(false);
+            this.tabNavigationPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            this.groupControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtdate.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.claclass.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvclassshedule)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -71,5 +227,18 @@
 
         private DevExpress.XtraBars.Navigation.TabPane tabPane1;
         private DevExpress.XtraBars.Navigation.TabNavigationPage tabNavigationPage3;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraEditors.Controls.CalendarControl claclass;
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.TextEdit txtdate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbbatch;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.DataGridView dgvclassshedule;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn batch;
+        private System.Windows.Forms.DataGridViewTextBoxColumn classdate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn state;
     }
 }
--- ProAdmin/ProAdmin/frmScheduleClass.cs
+++ ProAdmin/ProAdmin/frmScheduleClass.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using System.Data.Entity;
 
 namespace ProAdmin
 {
@@ -16,7 +17,13 @@ namespace ProAdmin
         public frmScheduleClass()
         {
             InitializeComponent();
+            get_combo_vale_list();
+            populate_shedule();
         }
+
+        basicdata_class model_class      = new basicdata_class();
+        data_attandance model_attandance = new data_attandance();
+        basicdata_student model_student  = new basicdata_student();
 
         private static frmScheduleClass _instance;
 
@@ -27,6 +34,144 @@ namespace ProAdmin
                 if (_instance == null)
                     _instance = new frmScheduleClass();
                 return _instance;
+            }
+        }
+
+        private void get_combo_vale_list()
+        {
+            using (proadmin_v1Entities db = new proadmin_v1Entities())
+            {
+                var batch = db.basicdata_batch.Select(y => new { y.batchid, y.batch });
+                cmbbatch.DataSource = batch.ToList();
+                cmbbatch.DisplayMember = "batch";
+                cmbbatch.SelectedItem = null;
+            }
+        }
+
+        private void populate_shedule()
+        {
+            using (proadmin_v1Entities db = new proadmin_v1Entities())
+            {
+                dgvclassshedule.DataSource = db.basicdata_class.ToList<basicdata_class>();
+            }
+        }
+
+        private void claclass_Click(object sender, EventArgs e)
+        {
+            txtdate.Text = claclass.SelectedRanges.Start.ToShortDateString();
+
+            using (proadmin_v1Entities db = new proadmin_v1Entities())
+            {
+                dgvclassshedule.DataSource = db.basicdata_class.Where(x => x.classdate == txtdate.Text).ToList<basicdata_class>();
+            }
+        }
+
+        private void clear_feild()
+        {
+            txtdate.Text = null;
+            cmbbatch.Text = null;
+        }
+
+        private void message_popup_ok(string messsage)
+        {
+            XtraMessageBoxArgs args = new XtraMessageBoxArgs();
+            args.Caption = "Notification!";
+            args.Text = messsage;
+            args.Buttons = new DialogResult[] { DialogResult.OK };
+            XtraMessageBox.Show(args).ToString();
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            if (txtdate.Text != "" && cmbbatch.Text != "")
+            {
+                model_class.batch       = cmbbatch.Text;
+                model_class.classdate   = txtdate.Text;
+                model_class.state       = "Active";
+
+                using (proadmin_v1Entities db = new proadmin_v1Entities())
+                {
+
+                    db.basicdata_class.Add(model_class);
+
+                    db.SaveChangesAsync();
+
+                    //student attdance empty records creating
+                    generate_attandance_records();
+
+                    message_popup_ok("Data Record Saved!");
+                    dgvclassshedule.DataSource = db.basicdata_class.Where(x => x.classdate == txtdate.Text).ToList<basicdata_class>();
+                    clear_feild();
+                }
+            }
+            else
+            {
+                message_popup_ok("Please fill all the fields!");
+            }
+        }
+
+        private void generate_attandance_records()
+        {
+            using (proadmin_v1Entities db = new proadmin_v1Entities())
+            {
+                var selectedStudents = db.basicdata_student.Where(i => i.Batch == cmbbatch.Text).ToList();
+
+                foreach (basicdata_student student in selectedStudents)
+                {
+                    model_attandance.classdate  = txtdate.Text;
+                    model_attandance.attandance = "0";
+                    model_attandance.batch      = cmbbatch.Text;
+                    model_attandance.intime     = "0";
+                    model_attandance.regid      = student.regid;
+
+                    db.data_attandance.Add(model_attandance);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            if (XtraMessageBox.Show("Are you sure to delete the record?", "Delete Record", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                using (proadmin_v1Entities db = new proadmin_v1Entities())
+                {
+                    var entity = db.Entry(model_class);
+                    if (entity.State == EntityState.Detached)
+                        db.basicdata_class.Attach(model_class);
+                    db.basicdata_class.Remove(model_class);
+                    db.SaveChangesAsync();
+
+                    btndelete.Enabled = false;
+
+                    if (XtraMessageBox.Show("Do you want to delete all the addtendance for this class shedule too?", "Authentication Required!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        var data = db.data_attandance.Where(rec => rec.batch == cmbbatch.Text && rec.classdate == txtdate.Text);
+                        db.data_attandance.RemoveRange(data);
+                        db.SaveChanges();
+                    }
+
+                    clear_feild();
+
+                    message_popup_ok("Data Deleted!");
+                }
+            }
+        }
+
+        private void dgvclassshedule_DoubleClick_1(object sender, EventArgs e)
+        {
+            if (dgvclassshedule.CurrentRow.Index != -1)
+            {
+                model_class.id = Convert.ToInt32(dgvclassshedule.CurrentRow.Cells["id"].Value);
+                using (proadmin_v1Entities db = new proadmin_v1Entities())
+                {
+                    model_class = db.basicdata_class.Where(x => x.id == model_class.id).FirstOrDefault();
+                    txtdate.Text  = model_class.classdate;
+                    cmbbatch.Text = model_class.batch;
+                    dgvclassshedule.DataSource = db.basicdata_class.Where(x => x.classdate == model_class.classdate).ToList<basicdata_class>();
+                }
+                btndelete.Enabled = true;
+                
             }
         }
     }
